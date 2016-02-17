52b2b8f6540ec37ae09c6e628af56309cf5b1c74
Karson Hatch <karsonhatch@gmail.com>
2016-02-17 11:19:23 -0700

    Added code to github.rb and some dope hastages to reademe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # First_GitHub
+# blacklivesmatter
+# sodoeverotherlifes
--- github.rb
+++ github.rb
@@ -2,3 +2,8 @@ puts "hello github"
 puts "What is your name?"
 
 name = gets.strip 
+if name == "Karson Hatch"
+	puts "Whaddup bruh, how's it hanging?"
+else
+	puts "Who dafuq are you?"
+end
